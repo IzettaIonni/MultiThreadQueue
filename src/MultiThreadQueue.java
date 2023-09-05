dbccb5ba5ac9bfa6bb6c76d3a0e2c5551e8effd3
Izetta <izettaioni@gmail.com>
2023-09-05 15:59:33 +0300

    1

--- src/MultiThreadQueue.java
+++ src/MultiThreadQueue.java
@@ -1,30 +1,41 @@
+import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MultiThreadQueue {
+public class MultiThreadQueue<T> {
 
     private final int limit;
-    private int[] queue;
     private boolean isLimitReached;
     private boolean isQueueEmpty;
-    private int front;
-    private int rear;
     private AtomicInteger actual;
 
     private final Object putLock = new Object();
     private final Object getLock = new Object();
+
+    private Node<T> head;
+    private Node<T> tail;
+
+    private static class Node<T> {
+        private final T value;
+        private Node<T> next;
+
+        Node(T value) {
+            this.value = value;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+    }
 
     MultiThreadQueue(int limit) throws Exception {
         if (limit != 0) {
             this.limit = limit;
             isLimitReached = false;
             isQueueEmpty = true;
-            queue = new int[limit];
-            front = 0;
-            rear = -1;
-            actual = 0;
+            actual = new AtomicInteger(0);
         }
         else {
             throw new Exception("Limit must be 1 or bigger");
@@ -32,7 +43,34 @@ public class MultiThreadQueue {
     }
 
     MultiThreadQueue() throws Exception {
-        this(9);
+        this(10);
+    }
+
+    public void offer(T obj) {
+        Node<T> next = new Node<T>(obj);
+
+        if (head == null) {
+            head = tail = next;
+        }
+        else {
+            tail.setNext(next);
+            tail = next;
+        }
+    }
+
+    public T poll() {
+        if (head == null) {
+            return null;
+        }
+        else {
+            T value = head.value;
+            head = head.next;
+            if (head == null) {
+                tail = null;
+            }
+            return value;
+        }
+
     }
 
     public  int get() throws Exception {
