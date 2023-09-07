11a1bdec822eb48009b34184b9b1fa6e03c3563b
Izetta <izettaionni@mail.ru>
2023-09-07 19:22:39 +0600

    MultiThreadQueue updated by pattern from LinkedBlockingQueue java source class

--- src/MultiThreadQueue.java
+++ src/MultiThreadQueue.java
@@ -1,13 +1,9 @@
-import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.locks.ReentrantLock;
 
 public class MultiThreadQueue<T> {
 
-//    private final int limit;
-//    private boolean isLimitReached;
-//    private boolean isQueueEmpty;
-    private AtomicInteger count;
+    private final int capacity;
+    private final AtomicInteger count;
 
     private final Object putLock = new Object();
     private final Object getLock = new Object();
@@ -16,27 +12,44 @@ public class MultiThreadQueue<T> {
     private Node<T> tail;
 
     private static class Node<T> {
-        private final T value;
+        private T value;
         private Node<T> next;
 
         Node(T value) {
             this.value = value;
         }
+    }
 
-        public void setNext(Node<T> next) {
-            this.next = next;
+    private void enqueue(Node<T> node) {
+        tail = tail.next = node;
+    }
+
+    private T dequeue() {
+        head = head.next;
+        T t = head.value;
+        head.value = null;
+        return t;
+    }
+
+    private void releaseGetLock() {
+        synchronized (getLock) {
+            getLock.notify();
         }
     }
 
-    MultiThreadQueue(int limit) throws Exception {
-        if (limit != 0) {
-//            this.limit = limit;
-//            isLimitReached = false;
-//            isQueueEmpty = true;
+    private void releasePutLock() {
+        synchronized (putLock) {
+            putLock.notify();
+        }
+    }
+
+    MultiThreadQueue(int size) throws IllegalArgumentException {
+        if (size != 0) {
+            capacity = size;
             count = new AtomicInteger(0);
         }
         else {
-            throw new Exception("Limit must be 1 or bigger");
+            throw new IllegalArgumentException("Limit must be 1 or bigger");
         }
     }
 
@@ -44,103 +57,34 @@ public class MultiThreadQueue<T> {
         this(10);
     }
 
-//    private void signalNotEmpty() {
-//        final ReentrantLock takeLock = this.takeLock;
-//        takeLock.lock();
-//        try {
-//            isQueueEmpty.signal();
-//        } finally {
-//            takeLock.unlock();
-//        }
-//    }
-//
-//    /**
-//     * Signals a waiting put. Called only from take/poll.
-//     */
-//    private void signalNotFull() {
-//        final ReentrantLock putLock = this.putLock;
-//        putLock.lock();
-//        try {
-//            .signal();
-//        } finally {
-//            putLock.unlock();
-//        }
-//    }
-
-    public void offer(T obj) {
-        final Node<T> next = new Node<T>(obj);
+    public void offer(T value) throws InterruptedException {
+        final int c;
         synchronized (putLock) {
-            if (head == null) {
-                head = tail = next;
+            while (count.get() == capacity) {
+                putLock.wait();
             }
-            else {
-                tail.setNext(next);
-                tail = next;
-            }
-            count.incrementAndGet();
+            enqueue(new Node<>(value));
+            c = count.incrementAndGet();
+            if (c < capacity)
+                putLock.notify();
         }
+        if (capacity == 1)
+            releaseGetLock();
     }
 
-    public T poll() {
-        if (head == null) {
-            return null;
-        }
+    public T poll() throws InterruptedException {
+        final T value;
         synchronized (getLock) {
-            if (head == null) {
-                return null;
+            while (count.get() == 0) {
+                getLock.wait();
             }
-            T value = head.value;
-            head = head.next;
-            if (head == null) {
-                tail = null;
-            }
+            value = dequeue();
             count.decrementAndGet();
-            return value;
-
-            T value = head.next.value;
-            head = head.next;
-            return value;
+            if (count.get() > 0)
+                releaseGetLock();
         }
+        if (count.get() < capacity)
+            releasePutLock();
+        return value;
     }
-
-    public  int get() throws Exception {
-        synchronized (getLock) {
-            System.out.println("---actual--- " + count);
-            if (count.get() == 0) isQueueEmpty = true;
-            while (isQueueEmpty) {
-                try {
-                    getLock.wait();
-                } catch (InterruptedException e) {
-                    throw new Exception("Empty wait is interrupted");
-                }
-            }
-            int x = queue[front];
-
-            front = (front + 1) % limit;
-            count.decrementAndGet();
-
-            isLimitReached = false;
-            getLock.notify();
-
-            return x;
-        }
-    }
-
-    public synchronized void put(int n) throws Exception {
-        if (count >= limit) isLimitReached = true;
-        while (isLimitReached) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                throw new Exception("Limit wait is interrupted");
-            }
-        }
-        rear = (rear + 1) % limit;
-        queue[rear] = n;
-        count++;
-
-        isQueueEmpty = false;
-        notify();
-    }
-
 }
