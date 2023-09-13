f5c11bd95700697b02787d28ca8f189beaf58f2d
Izetta <izettaionni@mail.ru>
2023-09-13 14:19:08 +0600

    single thread test and equal amount test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,5 @@
 public class Main {
-
-
-
     public static void main(String[] args) throws Exception {
-        MultiThreadQueue Q = new MultiThreadQueue();
-        Producer p1 = new Producer(Q);
-        Producer p2 = new Producer(Q);
-        Producer p3 = new Producer(Q);
-//        Producer p4 = new Producer(Q);
-        Consumer c1 = new Consumer(Q);
 
-        p1.start();
-        p2.start();
-        p3.start();
-//        p4.start();
-        c1.start();
     }
 }
